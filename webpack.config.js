77db32d0a680112a26945153ff012b69a016c758
Mark Wales <mark@smallhadroncollider.com>
2018-07-11 15:41:08 +0100

    refactor (webpack.config.js): switched over to webpack

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
+
+module.exports = {
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: "babel-loader",
+
+            options: {
+                presets: ["env"]
+            }
+        }]
+    },
+
+    plugins: [
+        new UglifyJSPlugin()
+    ],
+
+    entry: "./src/hsimp.js",
+
+    output: {
+        filename: "hsimp.min.js",
+        path: path.resolve(__dirname, "dist")
+    },
+
+    mode: "production"
+}
